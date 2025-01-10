f2a1246fb96f098c5a3a8abe497ba1fd551df778
willegge <willeggedev@gmail.com>
2025-01-10 16:43:46 -0500

    Wrap layout content in a Container component for improved structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Container from "@/components/Container";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -21,9 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Container>
+
         <Header />
         {children}
         <Footer />
+        </Container>
       </body>
     </html>
   );
