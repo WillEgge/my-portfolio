8d2364b6902864c558b56859139adbc13792a29a
willegge <willeggedev@gmail.com>
2025-01-13 11:38:58 -0500

    Update Container component to add top margin and remove background color

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,6 +1,6 @@
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-[1100px] mx-auto bg-white min-h-screen flex flex-col border-l border-r">
+    <div className="max-w-[1100px] mx-auto min-h-screen flex flex-col mt-10 border-l border-r border-transparent">
       {children}
     </div>
   );
